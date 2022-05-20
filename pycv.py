e95c183d221f19e004b9bda3af92c87b7ec6a569
CLazyCoder <xots899@naver.com>
2022-05-20 16:07:12 +0900

    메서드 추가.

--- pycv.py
+++ pycv.py
@@ -2,6 +2,7 @@ import numpy as np
 
 def convolution(img, kernel):
     res = img.copy()
+    assert kernel.shape[0] == kernel.shape[1] and kernel.shape[0] % 2 == 1, 'Wrong Kernel Size!'
     pass
 
 def rotate(img, angle):
@@ -11,4 +12,10 @@ def canny(img, t1, t2):
     pass
 
 def draw_line(img, x1, y1, x2, y2):
+    pass
+
+def calc_homography(points1, points2):
+    pass
+
+def calibrate_camera(img_points, obj_points):
     pass
